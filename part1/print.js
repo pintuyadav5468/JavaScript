3f9b0c3212975840ca93d1550aac039093b0a6b8
pintuyadav5468 <pintuyadav13624@gmail.com>
2025-04-06 11:19:57 +0530

    console added

--- part1/print.js
+++ part1/print.js
@@ -3,6 +3,9 @@ process.stdout.write("hello chai\n");
 console.table("test");
 console.table({name: "chai"});
 console.console.warn("warning");
+console.log("hello world", "hello chai", "test", {name: "chai"});
+
+
 
 
 
